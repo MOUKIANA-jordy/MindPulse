00b4809ff800a7bdf062910531ff1cbddfe227e0
MOUKIANA-jordy <jordymoukiana@gmail.com>
2025-02-07 13:57:19 +0100

    modification de index.tsx

--- frontend/mindpulse-app/app/(tabs)/index.tsx
+++ frontend/mindpulse-app/app/(tabs)/index.tsx
@@ -1,24 +1,19 @@
-import React, { useContext } from 'react';
+import React, { useContext} from 'react';
 import { AuthProvider, AuthContext } from '../../src/context/AuthContext'; // Assurez-vous que vous avez créé ce contexte
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { Stack } from "expo-router";
 import HomeScreen from '../../src/screens/HomeScreen'; // Page d'accueil
 import LoginScreen from '../../src/screens/LoginScreen'; // Page de login
 import RegisterScreen from '../../src/screens/RegisterScreen'; // Page d'inscription
 
-// Créer un StackNavigator
-const Stack = createStackNavigator();
 
-export default function App() {
+function AppNavigator() {
   const { user, loading } = useContext(AuthContext);
 
   // Si le contexte est en train de charger l'utilisateur, afficher un écran de chargement.
   if (loading) return null; // Tu peux aussi afficher un écran de chargement ici
 
   return (
-    <AuthProvider>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack screenOptions={{ headerShown: false }}>
           {user ? (
             // Si l'utilisateur est connecté, afficher l'écran principal (HomeScreen)
             <Stack.Screen name="Home" component={HomeScreen} />
@@ -29,8 +24,13 @@ export default function App() {
               <Stack.Screen name="Register" component={RegisterScreen} />
             </>
           )}
-        </Stack.Navigator>
-      </NavigationContainer>
+        </Stack>
+      );
+    }
+    export default function App() {
+      return (
+        <AuthProvider>
+          <AppNavigator />
     </AuthProvider>
   );
 }
